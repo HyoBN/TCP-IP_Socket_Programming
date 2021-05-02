912db962cd632914e8b8271b36c615034f368c26
양효빈 <qls0786@naver.com>
2021-05-02 14:48:48 +0000

    Calculator server implementing.....

--- Chap5/Calc_server.c
+++ Chap5/Calc_server.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	int recv_len=0,recv_cnt;
 	int num_cnt=0;
 	int answer=0;
-	int nums[BUF_SIZE];
+	int data[BUF_SIZE];
 	
 	char message[BUF_SIZE];
 	
@@ -56,38 +56,37 @@ int main(int argc, char *argv[])
 	
 	
 	
-	while(num_cnt*NUM_SIZE >= recv_len) // (전달받은 데이터 수(num_cnt) * 데이터 하나의 크기(NUM_SIZE)) ??????????
+	while(num_cnt*NUM_SIZE+1 > recv_len) // (연산할 숫자 개수 * 숫자 하나 크기 + 연산자 하나 크기) > recv_len.
+
+		// number과 operator를 구분하는 게 아니라 하나의 데이터 블럭에 연속적으로 전달되는 형태에서 read를 끊어서 받는 구조!!
 		
 	{
-		recv_cnt=read(clnt_sock, &nums[recv_len], BUF_SIZE-1); //숫자 입력받기. / read 함수는 호출성공하면 데이터의 크기(int 4)를 반환한다.
+		recv_cnt=read(clnt_sock, &data[recv_len], BUF_SIZE-1); // read 함수는 호출성공하면 데이터의 크기(int : 4, char : 1)를 반환한다.
 		if(recv_cnt==-1)
 			error_handling("read() error");
 		recv_len+=recv_cnt;
 	}
 	
-	read(clnt_sock, message, BUF_SIZE); // 연산자 입력받기.
 	
+	// 교재 코드에서 61번째줄 opnds의 index가 1씩 증가하면 입력받은 숫자들에 접근?
 	
-	
-	
-	
-	if(message == '+')
+	if(data[recv_len-1] == '+')
 	{
 		for(int i=0;i<num_cnt;i++)
-			answer+=nums[i];
+			answer+=data[i];
 	}
 	
-	else if(message == '-')
+	else if(data[recv_len-1] == '-')
 	{
 		for(int i=0;i<num_cnt;i++)
-			answer-=nums[i];
+			answer-=data[i];
 	}
 	
-	else if(message == '*')
+	else if(data[recv_len-1] == '*')
 	{
 		answer=1;
 		for(int i=0;i<num_cnt;i++)
-			answer*=nums[i];
+			answer*=data[i];
 	}
 	
 	write(clnt_sock, ("Operation result : ",answer), str_len);
