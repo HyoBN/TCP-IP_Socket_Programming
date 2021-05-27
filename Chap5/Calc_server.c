f3c756282972d734b1db52775a3f2f4c6bb37031
양효빈 <qls0786@naver.com>
2021-05-27 11:15:59 +0000

    계산기 server

--- Chap5/Calc_server.c
+++ Chap5/Calc_server.c
@@ -8,20 +8,20 @@
 #define BUF_SIZE 1024
 #define NUM_SIZE 4
 void error_handling(char *message);
+int calculate(int opnum, int opnds[], char oprator);
 
 int main(int argc, char *argv[])
 {
 	int serv_sock, clnt_sock;
-	int recv_len=0,recv_cnt;
+	char data[BUF_SIZE];
 	int num_cnt=0;
 	int answer=0;
-	int data[BUF_SIZE];
-	
+	int recv_len=0,recv_cnt;
 	char message[BUF_SIZE];
 	
-	struct sockaddr_in serv_addr, clnt_addr;
+	struct sockaddr_in serv_adr, clnt_adr;
+	socklen_t clnt_adr_sz;
 	
-	// base code.
 	
 	if(argc!=2){
 		printf("Usage : %s <port>\n",argv[0]);
@@ -42,21 +42,15 @@ int main(int argc, char *argv[])
 	
 	if(listen(serv_sock, 5)==-1)
 		error_handling("listen() error");
-	
 	clnt_adr_sz=sizeof(clnt_adr);
+	//~~~~~~~~~~~~~~~~~여기까지는 base code라 문제x.
+	
 	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
 	read(clnt_sock, &num_cnt, 1); // 연산할 숫자의 개수를 전달받아 num_cnt에 저장.
 	
+		
 	
-	
-	if(clnt_sock==-1)
-		error_handling("accept() error");
-	else
-		printf("Connected...... \n", i+1);
-	
-	
-	
-	while(num_cnt*NUM_SIZE+1 > recv_len) // (연산할 숫자 개수 * 숫자 하나 크기 + 연산자 하나 크기) > recv_len.
+	while(num_cnt*NUM_SIZE+1 > recv_len) // (연산할 숫자 개수 * 숫자 하나 크기 + 연산자 하나 크기) > recv_len. , 
 
 		// number과 operator를 구분하는 게 아니라 하나의 데이터 블럭에 연속적으로 전달되는 형태에서 read를 끊어서 받는 구조!!
 		
@@ -65,41 +59,46 @@ int main(int argc, char *argv[])
 		if(recv_cnt==-1)
 			error_handling("read() error");
 		recv_len+=recv_cnt;
+		
+		answer=calculate(num_cnt, (int*)data, data[recv_len-1]);
+		write(clnt_sock, (char*)&answer,sizeof(answer));
+		close(clnt_sock);
 	}
 	
+}
 	
-	// 교재 코드에서 61번째줄 opnds의 index가 1씩 증가하면 입력받은 숫자들에 접근?
-	
-	if(data[recv_len-1] == '+')
+
+int calculate(int opnum, int opnds[], char op)
+{
+	int result=opnds[0],i;
+	switch(op)
 	{
-		for(int i=0;i<num_cnt;i++)
-			answer+=data[i];
+		case '+':
+			for(i=1;i<opnum;i++) result +=opnds[i];
+			break;
+		case '-':
+			for(i=1;i<opnum;i++) result -=opnds[i];
+			break;
+		case '*':
+			for(i=1;i<opnum;i++) result *=opnds[i];
+			break;
+
 	}
+	return result;
+}
+
+
+
+
 	
-	else if(data[recv_len-1] == '-')
-	{
-		for(int i=0;i<num_cnt;i++)
-			answer-=data[i];
-	}
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n',stderr);
+	exit(1);
+}
 	
-	else if(data[recv_len-1] == '*')
-	{
-		answer=1;
-		for(int i=0;i<num_cnt;i++)
-			answer*=data[i];
-	}
-	
-	write(clnt_sock, ("Operation result : ",answer), str_len);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	
